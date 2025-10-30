f7e713a48b6cb0b800d14516af2296f533cff44c
Svinte <99759858+Svinte@users.noreply.github.com>
2025-10-30 15:00:27 +0200

    Add already used word filter

--- src/main.py
+++ src/main.py
@@ -1,15 +1,21 @@
 from _filter import filter_words, filter_candidates_by_comparison
 from _common import top_letters
 from _score import score_words
+import csv
 
 ANSWER_LIST_PATH = 'storage/public/wordlist.txt'
 QUESS_LIST_PATH = 'storage/public/wordlist2.txt'
+PAST_ANSWERS_PATH = 'storage/public/wordle_answers.csv'
 
 with open(QUESS_LIST_PATH, "r", encoding="utf-8") as f:
     all_possible_inputs = [w.strip().lower() for w in f if len(w.strip()) == 5]
 
 with open(ANSWER_LIST_PATH, "r", encoding="utf-8") as f:
     answers = [w.strip().lower() for w in f if len(w.strip()) == 5]
+
+with open(PAST_ANSWERS_PATH, "r", encoding="utf-8") as f:
+    reader = csv.DictReader(f)
+    past_answers = {row['answer'].strip().lower() for row in reader}
 
 # Previous guesses and their results
 # g = green, y = yellow, b = gray
@@ -26,6 +32,7 @@ contains = [
 ]
 
 candidates = [ans for ans in answers if all(k in ans for k in contains)]
+candidates = [w for w in candidates if w not in past_answers]
 candidates = filter_words(candidates, guesses)
 candidates = filter_candidates_by_comparison(candidates, all_possible_inputs, comparing)
 
